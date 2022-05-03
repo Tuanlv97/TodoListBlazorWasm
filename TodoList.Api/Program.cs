ed41b8606169155fd7ca537ab80c3f8bc5fed5b8
DESKTOP-G0ON5RS\DEV37 <Anhtuan20011997@gmail.com>
2022-05-03 12:37:39 +0700

    Seed Data

--- TodoList.Api/Program.cs
+++ TodoList.Api/Program.cs
@@ -1,11 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using TodoList.Api.Data;
+using TodoList.Api.Extensions;
 
 namespace TodoList.Api
 {
@@ -13,7 +11,14 @@ namespace TodoList.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            host.MigrateDbContext<TodoListDbContext>((context, services) =>
+            {
+                var logger = services.GetService<ILogger<TodoListDbContextSeed>>();
+                new TodoListDbContextSeed().SeedAsync(context, logger).Wait();
+            });
+            host.Run();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
